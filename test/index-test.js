afd30931e3b18861d7514274ec76c248b4746817
Supaasara Bowman <supaasarabowman@Supaasaras-MacBook-Pro.local>
2022-02-10 17:51:03 -0700

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -5,11 +5,25 @@ describe('shout(string)', function() {
   })
 })
 
+function shout(string) {
+  // todo
+}
+function shout(string) {
+  return string.toUpperCase();
+}
+
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
   })
 })
+
+function whisper(string) {
+  // todo
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
 
 describe('logShout(string)', function() {
   it('takes a string argument and logs it in all caps using console.log()', function() {
@@ -23,6 +37,13 @@ describe('logShout(string)', function() {
   })
 })
 
+function logShout(string) {
+  // todo
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
 describe('logWhisper(string)', function() {
   it('takes a string argument and logs it in all lowercase using console.log()', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough();
@@ -35,10 +56,30 @@ describe('logWhisper(string)', function() {
   })
 })
 
+function logWhisper(string) {
+  // todo
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
 describe('sayHiToHeadphonedRoommate(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
   })
+
+  function sayHiToHeadphonedRoommate(string) {
+    // todo
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase())
+        return "I can't hear you!"
+    else if (string === string.toUpperCase())
+        return "YES INDEED!"
+    else if (string ===  "Let's have dinner together!")
+        return "I would love to!"
+
+  }  
 
   it('returns "YES INDEED!" if `string` is uppercase', function() {
     expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
